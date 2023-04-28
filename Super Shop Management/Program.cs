820f4bd2ece64447bd39c015c0c4de901c817def
Mrittika, Wasifa Tasnim <21-45108-2@student.aiub.edu>
2023-04-28 16:43:36 +0600

    change

--- Super Shop Management/Program.cs
+++ Super Shop Management/Program.cs
@@ -21,7 +21,7 @@ namespace Super_Shop_Management
             //Application.Run(new Admin_itemInfo());
             //Application.Run(new Admin_empInfo());
             //Application.Run(new CustomerView());
-            //Application.Run(new Emp_itemInfo());
+            Application.Run(new Emp_itemInfo());
             //Application.Run(new Registration_Form());
 
         }
